eef2b8e8c926f298b66342603ba9f57a96e0ca07
Artem Yudin <yudin.artem.90@yandex.ru>
2020-11-30 00:03:45 +0300

    fix mongo

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,8 @@ const limiter = rateLimit({
   max: 100,
 });
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+// mongoose.connect('mongodb://localhost:27017/mestodb', {
+  mongoose.connect('mongodb://artem.students.nomoredomains.monster:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
