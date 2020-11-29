4ffb43db5ff27eda6e7d7e0e1ecd569b27022984
Artem Yudin <yudin.artem.90@yandex.ru>
2020-11-29 14:40:24 +0300

    фикс валидатора

--- backend/helpers/constants/index.js
+++ backend/helpers/constants/index.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-useless-escape
-// const regExpUrl = '^(http|https):\/\/([A-Za-z]|\.|[0-9]|\/|\#|\-|\_)+';
-const regExpUrl = '^(http|https):\/\/([A-Za-z]|\.|[0-9]|\/|\#|\-|\_)+\.(jpg|png|jpeg|tiff)$';
+const regExpUrl = '^(http|https):\/\/([A-Za-z]|\.|[0-9]|\/|\#|\-|\_)+(jpg|png|jpeg|gif)';
+
 module.exports = {
   regExpUrl,
 };
--- backend/helpers/validation.js
+++ backend/helpers/validation.js
@@ -2,6 +2,5 @@ const { regExpUrl } = require('./constants/index');
 
 module.exports.validationUrl = (url) => {
   const regExp = new RegExp(regExpUrl, 'g');
-  regExp.test(url);
-  return regExp.lastIndex === url.length;
+  return regExp.test(url);
 };
