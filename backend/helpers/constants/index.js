0865d9a8b3f5285e5de493169531dd738997a785
artemYudin041290 <artem.yudin@mediadirectiongroup.ru>
2020-12-02 15:17:10 +0300

    исправил шибки линтера

--- backend/helpers/constants/index.js
+++ backend/helpers/constants/index.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-useless-escape
 const regExpUrl = '^(http|https):\/\/([A-Za-z]|\.|[0-9]|\/|\#|\-|\_)+';
 const regExpObjectId = '^[0-9a-fA-F]{24}$';
+// eslint-disable-next-line no-useless-escape
 const regExpEmail = '^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$';
 
 module.exports = {
