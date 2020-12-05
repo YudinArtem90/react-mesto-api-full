920c90e7c24ea7019639c8d2cd5643166b01a227
artemYudin041290 <artem.yudin@mediadirectiongroup.ru>
2020-12-05 22:41:08 +0300

    фикс фронта

--- frontend/src/utils/workingWithUser.js
+++ frontend/src/utils/workingWithUser.js
@@ -1,18 +1,11 @@
 import Ajax from './ajax';
-import workingWithToken from './workingWithToken'
-
 class WorkingWithUser extends Ajax{
-    constructor({baseUrl, groupId, authorization}){
+    constructor({baseUrl}){
         super(baseUrl);
-        this._groupId = groupId;
-        super._data = {
-            headers: {
-                authorization: `Bearer ${workingWithToken.getToken()}`
-            }
-        };
     }
 
     getUserInfo(){
+        super._getData({});
         return fetch(`${this._baseUrl}/users/me`, this._data)
                     .then(res => { return super._getResult(res) });
     }
